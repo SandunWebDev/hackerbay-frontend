ddad41f9e6ec8610447a350c8dddae06430636ed
SandunWebDev <sandunwebdev@gmail.com>
2018-09-15 11:34:46 +0530

    Added simple signup form to SignupPage component.

--- src/components/pages/SignupPage/SignupPage.js
+++ src/components/pages/SignupPage/SignupPage.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 
 export default class SignupPage extends Component {
   render() {
-    return <h1>Signup Page</h1>;
+    return (
+      <div>
+        <h1>Signup Page</h1>
+        <form name="signup_form">
+          <input type="text" name="name" placeholder="Your Name" />
+          <input type="email" name="email" placeholder="Your Valid Email" />
+          <input type="password" name="password" placeholder="Your Password" />
+          <input
+            type="password"
+            name="password_confirm"
+            placeholder="Retype Your Password"
+          />
+          <button>Create My Account</button>
+        </form>
+      </div>
+    );
   }
 }
