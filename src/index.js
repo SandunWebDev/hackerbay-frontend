d6dccc2a521fb6023003abac7aee42da715d1dcb
SandunWebDev <sandunwebdev@gmail.com>
2018-09-15 10:40:30 +0530

    Configured ESLint and Prettier.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import registerServiceWorker from './registerServiceWorker';
-import App from './components/App/App';
-import './index.css';
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./components/App/App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 registerServiceWorker();
