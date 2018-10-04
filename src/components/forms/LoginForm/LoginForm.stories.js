dbf6e84f018fc171d0553254dc2a80265d642c97
SandunWebDev <sandunwebdev@gmail.com>
2018-10-04 08:00:24 +0530

    ReduxForm story problem

--- src/components/forms/LoginForm/LoginForm.stories.js
+++ src/components/forms/LoginForm/LoginForm.stories.js
@@ -4,11 +4,37 @@ import { storiesOf } from "@storybook/react";
 import injectReduxProvider from "../../../stories/customDecorators/injectReduxProvider";
 import injectReactRouter from "../../../stories/customDecorators/injectReactRouter";
 import LoginForm from "./LoginForm";
-import { LoginForm as ABC } from "./LoginForm";
+
+import { reduxForm } from "redux-form";
+import {
+  LoginForm as LoginFormWithoutWrapper,
+  reduxFormConfig
+} from "./LoginForm";
 
 storiesOf("Forms/LoginForm", module)
   .addDecorator(injectReduxProvider())
   .addDecorator(injectReactRouter())
   .add("Initial View", () => {
     return <LoginForm />;
+  })
+  .add("When Form Is Submitting", () => {
+    const MockedLoginForm = reduxForm({
+      ...reduxFormConfig,
+      destroyOnUnmount: false,
+      keepDirtyOnReinitialize: true
+    })(LoginFormWithoutWrapper);
+
+    const myProps = {
+      user: {
+        loggedIn: false
+      },
+      userActions: { loginAccount: () => {} },
+
+      initialValues: { email: "a@a.com", password: "pppp" },
+      submitting: true,
+      pristine: false,
+      pure: false
+    };
+
+    return <MockedLoginForm {...myProps} />;
   });
