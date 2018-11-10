7aa797ea482d0e0aa8d9724456814e358f43ebf0
SandunWebDev <sandunwebdev@gmail.com>
2018-11-10 08:32:38 +0530

    Added test cases for reduxFormValidator().

--- src/components/forms/AuthHelperForm/AuthHelperForm.test.js
+++ src/components/forms/AuthHelperForm/AuthHelperForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { shallow } from "enzyme";
 
 import CustomInputWithErrorOutput from "../customInputs/CustomInputWithErrorOutput/CustomInputWithErrorOutput";
 import { Field } from "redux-form";
